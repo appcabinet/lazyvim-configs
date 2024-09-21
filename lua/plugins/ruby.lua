26bbde4c6483d9b2780617d42bfeee56c81a6b32
Marcelo Mantilla <marcelo.mantilla@appcabinet.com>
2024-09-20 21:30:39 -0400

    feat: Update plugin commits in lazy-lock.json

--- lua/plugins/ruby.lua
+++ lua/plugins/ruby.lua
@@ -2,9 +2,10 @@ return {
   {
     "nvim-treesitter/nvim-treesitter",
     opts = function(_, opts)
-      -- add tsx and treesitter
       vim.list_extend(opts.ensure_installed, {
         "ruby",
+        "erb",
+        "html",
       })
     end,
   },
@@ -13,26 +14,6 @@ return {
     opts = {
       servers = {
         ruby_lsp = {},
-        ruby_ls = {},
-
-        rubocop = {
-          cmd = { "bundle", "exec", "rubocop", "--lsp" },
-          root_dir = require("lspconfig").util.root_pattern("Gemfile", ".git"),
-        },
-      },
-    },
-  },
-  {
-    "nvim-neotest/neotest",
-    optional = true,
-    dependencies = {
-      "olimorris/neotest-rspec",
-    },
-    opts = {
-      adapters = {
-        ["neotest-rspec"] = {
-          -- Configure neotest-rspec here
-        },
       },
     },
   },
