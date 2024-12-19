7e51064e74074c9be05fec0d8d77f333f7d859ea
Marcelo Mantilla <marcelo.mantilla@appcabinet.com>
2024-12-19 18:38:09 -0500

    feat: Update commit hashes for various plugins

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,3 +3,21 @@
 -- Add any additional options here
 --
 vim.g.lazyvim_ruby_formatter = "rubocop"
+
+vim.opt.scrolloff = 18
+
+vim.g.lazyvim_picker = "telescope"
+
+opts = function()
+  if LazyVim.pick.want() ~= "telescope" then
+    return
+  end
+  local Keys = require("lazyvim.plugins.lsp.keymaps").get()
+  -- stylua: ignore
+  vim.list_extend(Keys, {
+    { "gd", function() require("telescope.builtin").lsp_definitions({ reuse_win = true }) end, desc = "Goto Definition", has = "definition" },
+    { "gr", "<cmd>Telescope lsp_references<cr>", desc = "References", nowait = true },
+    { "gI", function() require("telescope.builtin").lsp_implementations({ reuse_win = true }) end, desc = "Goto Implementation" },
+    { "gy", function() require("telescope.builtin").lsp_type_definitions({ reuse_win = true }) end, desc = "Goto T[y]pe Definition" },
+  })
+end
